abcd68f45e42a9374ac9a91665eb87d8f1cf2637
mirco_cesaretto <mirco.cesaretto95@gmail.com>
2021-11-03 17:28:02 +0100

    export components updated and version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import {CrossImage }from "mini-cross-components"
 
 function App() {
   return (
     <div className="App">
-      
+      <CrossImage/>
     </div>
   );
 }
